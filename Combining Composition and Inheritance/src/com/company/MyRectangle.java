f45b264d442e771a91b988c7df22e998a0a6fc4a
Pascal Kollie <aldavekollie@gmail.com>
2018-03-20 21:51:19 -0400

    Add files via upload

---
+++ Combining Composition and Inheritance/src/com/company/MyRectangle.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class MyRectangle extends MyBoundedShape
+{
+    public MyRectangle(Color c)
+    {
+        super(c);
+    }
+
+    @Override
+    public void draw(Graphics g)
+    {
+        g.setColor(getColor());
+
+        if (getFilled())
+            g.fillRect(getComponentX(), getComponentY(), getWidth(), getHeight());
+        else
+            g.drawRect(getComponentX(), getComponentY(), getWidth(), getHeight());
+    }
+}
+
+
---
+++ Combining Composition and Inheritance/src/com/company/MyShape.java
@@ -0,0 +1,8 @@
+package com.company;
+
+import java.awt.Graphics;
+
+public abstract class MyShape {
+
+    public abstract void draw(Graphics g);
+}
